6a89656afa30334fa360747a1fba649029e84390
Sreejit <sreejit9821@gmail.com>
2022-09-25 20:31:43 +0530

    Logout button and font awesome added

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -68,24 +68,10 @@ AppAsset::register($this);
         <ul class="navbar-nav ml-auto">
             <!-- Navbar Search -->
             <li class="nav-item">
-                <a class="nav-link" data-widget="navbar-search" href="#" role="button">
-                    <i class="fas fa-search"></i>
+                <a class="nav-link"  href=<?=Yii::$app->urlManager->createAbsoluteUrl('site/logout') ?>>
+                    <i class="fas fa-power-off"></i>
                 </a>
-                <div class="navbar-search-block">
-                    <form class="form-inline">
-                        <div class="input-group input-group-sm">
-                            <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
-                            <div class="input-group-append">
-                                <button class="btn btn-navbar" type="submit">
-                                    <i class="fas fa-search"></i>
-                                </button>
-                                <button class="btn btn-navbar" type="button" data-widget="navbar-search">
-                                    <i class="fas fa-times"></i>
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+
             </li>
 
         </ul>
